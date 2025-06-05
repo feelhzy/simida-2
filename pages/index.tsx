3b4b6140e9527bf49035e4ba80056d8a81d46b98
feelhzy <138498121+feelhzy@users.noreply.github.com>
2025-06-06 03:18:52 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">欢迎来到 Smida 社区平台！</h1>
+      <h1 className="text-4xl font-bold text-purple-600">欢迎来到 Smida 2.0 进阶课堂 ✨</h1>
     </div>
   );
 }
